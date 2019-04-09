7c47a44b0404422c906dfc706878667944adf66b
colineanb <coline.animbo@gmail.com>
2019-04-09 11:29:55 +0200

    :sparkles: add front home and header

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,5 +1,14 @@
 <?php include '../views/partials/header.php' ?>
 
-<h1>Yo</h1>
+    <div class="container">
+        <div class="content">
+            <h1>Défis tes amis sur PEA</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit a augue sit amet porttitor. Mauris sit amet urna euismod, molestie eros sed, interdum lectus. Praesent sit amet tortor sollicitudin, eros malesuada tortor sit amet, porta sit amet a erat. </p>
+            <a href="#">Je crée mon compte</a>
+        </div>
+        <div class="illustration">
+            <img src="<?=URL?>assets/images/img-1.svg" alt="foot">
+        </div>
+    </div>
 
 <?php include '../views/partials/footer.php' ?>
--- views/partials/header.php
+++ views/partials/header.php
@@ -1,1 +1,24 @@
-<h1>Hello</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= !empty($title)? $title: ''?></title>
+    <link rel="stylesheet" href="<?=URL?>assets/css/style.css">
+    <link rel="stylesheet" href="<?=URL?>assets/css/reset.css">
+    <link rel="stylesheet" href="<?=URL?>assets/css/league.css">
+    <link href="https://fonts.googleapis.com/css?family=Amaranth|Titillium+Web" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="index.html" class="title-nav">Pea</a>
+            <ul>
+                <li><a href=""> Créer ma ligue</a></li>
+                <li><a href=""> Mes ligues</a></li>
+                <li><a href=""><img src="<?=URL?>assets/images/icon-1.png" alt=""></a></li>
+                <li><a href=""><img src="<?=URL?>assets/images/icon-2.png" alt=""></a></li>
+            </ul>
+        </nav>
+    </header>
