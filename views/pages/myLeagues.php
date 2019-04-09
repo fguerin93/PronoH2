077a2cd38723d51746faaf6b7f456bc4a9e4e257
QuentinJezequel <quempic2@gmail.com>
2019-04-09 18:10:51 +0200

    changes to myLeague.php / myLeague.css
    This reverts commit 8496fe454122fc6c2561e1c280479a2d070c0b39.

--- views/pages/myLeagues.php
+++ views/pages/myLeagues.php
@@ -1,75 +1,76 @@
 <?php include 'header.php'; ?>
-
-<div class="principal">
-  <div class="content">
+<head>
+  <link rel="stylesheet" href="css/myLeagues.css">
+</head>
+<div class="myleague-principal">
+  <div class="myleague-content">
     <h1>Mes ligues</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
   </div>
-<div class="globalSquares">
+<div class="myleague-globalSquares">
 
-  <div class="leaguesSquares1">
+  <div class="myleague-leaguesSquares1">
 
-    <div class="square1">
-      <div class="square1Logo">
+    <div class="myleague-square1">
+      <div class="myleague-square1Logo">
         <img src="images/PSG.png" alt="écusson PSG">
         <h2>Hétic Football</h2>
       </div>
-      <button class="stateButton "type="button" name="button">En cours</button>
+      <button class="myleague-stateButton "type="button" name="button">En cours</button>
     </div>
-    <div class="square1">
-      <div class="square1Logo">
+    <div class="myleague-square1">
+      <div class="myleague-square1Logo">
         <img src="images/PSG.png" alt="écusson PSG">
         <h2>Hétic Football</h2>
       </div>
-      <button class="stateButton "type="button" name="button">En cours</button>
+      <button class="myleague-stateButton "type="button" name="button">En cours</button>
     </div>
-    <div class="square1">
-      <div class="square1Logo">
+    <div class="myleague-square1">
+      <div class="myleague-square1Logo">
         <img src="images/PSG.png" alt="écusson PSG">
         <h2>Hétic Football</h2>
       </div>
-      <button class="stateButton "type="button" name="button">En cours</button>
+      <button class="myleague-stateButton "type="button" name="button">En cours</button>
     </div>
-    <div class="square1">
-      <div class="square1Logo">
+    <div class="myleague-square1">
+      <div class="myleague-square1Logo">
         <img src="images/PSG.png" alt="écusson PSG">
         <h2>Hétic Football</h2>
       </div>
-      <button class="stateButton "type="button" name="button">En cours</button>
+      <button class="myleague-stateButton "type="button" name="button">En cours</button>
     </div>
   </div>
 
-  <div class="leaguesSquares2">
+  <div class="myleague-leaguesSquares2">
 
-    <div class="square2">
-      <div class="square2Logo">
+    <div class="myleague-square2">
+      <div class="myleague-square2Logo">
         <img src="images/PSG.png" alt="écusson PSG">
         <h2>Hétic Football</h2>
       </div>
-      <button class="stateButton "type="button" name="button">En cours</button>
+      <button class="myleague-stateButton "type="button" name="button">En cours</button>
     </div>
-    <div class="square2">
-      <div class="square2Logo">
+    <div class="myleague-square2">
+      <div class="myleague-square2Logo">
         <img src="images/PSG.png" alt="écusson PSG">
         <h2>Hétic Football</h2>
       </div>
-      <button class="stateButton "type="button" name="button">En cours</button>
+      <button class="myleague-stateButton "type="button" name="button">En cours</button>
     </div>
-    <div class="square2">
-      <div class="square2Logo">
+    <div class="myleague-square2">
+      <div class="myleague-square2Logo">
         <img src="images/PSG.png" alt="écusson PSG">
         <h2>Hétic Football</h2>
       </div>
-      <button class="stateButton "type="button" name="button">En cours</button>
+      <button class="myleague-stateButton "type="button" name="button">En cours</button>
     </div>
-    <div class="square2">
-      <div class="square2Logo">
+    <div class="myleague-square2">
+      <div class="myleague-square2Logo">
         <img src="images/PSG.png" alt="écusson PSG">
         <h2>Hétic Football</h2>
       </div>
-      <button class="stateButton "type="button" name="button">En cours</button>
+      <button class="myleague-stateButton "type="button" name="button">En cours</button>
     </div>
   </div>
 </div>
-
 </div>
