39ac5f9a5299a7e57e8b65673ebc498f7d574a4f
colineanb <coline.animbo@gmail.com>
2019-04-10 19:06:53 +0200

    css

--- views/pages/home2.php
+++ views/pages/home2.php
@@ -5,12 +5,17 @@
             <h1>Pronos entre amis</h1>
             <p>Tu peux créer ta propre ligue ou rejoindre la ligue de tes amis/collègues</p>
             <div class="league-container">
-                <?php foreach ($leagues as $league) { ?>
-                    <div class="league">
-                        <h2 class="league-name"><?= $league->name ?></h2>
-                        <button class="stateButton "type="button" name="button">En cours</button>
-                    </div>
-                <?php } ?>
+                <div class="league">
+                    <h2 class="league-name">Créer une ligue</h2>
+                    <button class="stateButton "type="button" name="button">Créer</button>
+                </div>
+                <div class="league">
+                    <form action="" methode="POST">
+                        <label for="name">Rejoindre une ligue</label><br>
+                        <input type="text" name="code" placeholder="Rentre le code">
+                    </form>
+                    <button type="submit" name="formcreationleague">OK</button>
+                </div>
             </div>
         </div>
     </div>
