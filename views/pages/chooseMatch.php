b8098a922650b746e1626c6a09c619eac92977f9
colineanb <coline.animbo@gmail.com>
2019-04-10 11:17:50 +0200

    add choose match

--- views/pages/chooseMatch.php
+++ views/pages/chooseMatch.php
@@ -2,7 +2,17 @@
 include '../views/partials/header.php';
 ?>
 
-<h1>choose match</h1>
+<h1>Choisis tes matchs</h1>
+<div class="selected-match">
+    <label for="choose-match">Choisi tes matchs:</label>
+
+    <select name="comp" onchange="this.form.submit();">
+        <option value="">--Trie tes ligues--</option>
+        <option value="FL1">Ligue 1</option>
+        <option value="PL">Première ligue</option>
+    </select>
+</div>
+
 
 <?php 
 include '../views/partials/footer.php';
