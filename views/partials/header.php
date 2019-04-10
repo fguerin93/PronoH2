2477b0638dd98d48e476fa55d030168fd1841f84
QuentinJezequel <quempic2@gmail.com>
2019-04-10 17:45:50 +0200

    miss "assests" in  link of header.php

--- views/partials/header.php
+++ views/partials/header.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="<?=URL?>assets/css/myLeagues.css">
     <link rel="stylesheet" href="<?=URL?>assets/css/inscription.css">
     <link rel="stylesheet" href="<?=URL?>assets/css/chooseMatch.css">
-    <link rel="stylesheet" href="<?=URL?>css/mesPronos.css">
+    <link rel="stylesheet" href="<?=URL?>assets/css/mesPronos.css">
     <link href="https://fonts.googleapis.com/css?family=Amaranth|Titillium+Web" rel="stylesheet">
 </head>
 <body>
