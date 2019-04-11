fee5595c400c7e7c1e9fbdda9af66f62367a91cf
colineanb <coline.animbo@gmail.com>
2019-04-11 21:18:25 +0200

    :sparkles: add ranking

---
+++ views/pages/ranking.php
@@ -0,0 +1,39 @@
+     <div class="ranking-container">
+        <div class="ranking-content">
+            <h1>Hetic Football</h1>
+            <div class="rank-row">
+                <h2>Classement en direct</h2>
+                <a href=""> Voir les pronostics</a>
+            </div>
+                <table>
+                    <tr>
+                        <th class="left-corner">Rang</th>
+                        <th>Joueurs</th>
+                        <th>Bon pronos</th>
+                        <th>Exacts</th>
+                        <th class="right-corner">Points</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td><?= $_SESSION['pseudo'] ?></td>
+                        <td>22</td>
+                        <td>43</td>
+                        <td>90</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Todd</td>
+                        <td>22</td>
+                        <td>43</td>
+                        <td>90</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Todd</td>
+                        <td>22</td>
+                        <td>43</td>
+                        <td>90</td>
+                    </tr>
+                </table>
+        </div>   
+    </div>
